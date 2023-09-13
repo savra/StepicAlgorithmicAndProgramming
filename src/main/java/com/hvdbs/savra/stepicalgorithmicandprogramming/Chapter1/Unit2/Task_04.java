42b199586c86815d57c4e851a936e7f863d032b9
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-13 17:54:41 +0300

    solution for task 1.2.4

---
+++ src/main/java/com/hvdbs/savra/stepicalgorithmicandprogramming/Chapter1/Unit2/Task_04.java
@@ -0,0 +1,33 @@
+package com.hvdbs.savra.stepicalgorithmicandprogramming.Chapter1.Unit2;
+
+import java.util.Scanner;
+
+/**
+ На вход подаётся одна строка длиною не более 100 символов.
+ На выход подайте перевёрнутую строку.
+
+ Например.
+ Вход:
+ Строка
+ Выход:
+ акортС
+
+ Sample Input 1:
+ потоп
+ Sample Output 1:
+ потоп
+
+ Sample Input 2:
+ Питон
+ Sample Output 2:
+ нотиП
+*/
+public class Task_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String source = scanner.next();
+        StringBuilder sb = new StringBuilder(source);
+
+        System.out.println(sb.reverse());
+    }
+}
