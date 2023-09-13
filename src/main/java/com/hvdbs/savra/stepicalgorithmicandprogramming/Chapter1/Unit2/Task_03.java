a07e2193996f65f48270429c2680c976287e310c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-13 17:51:20 +0300

    solution for task 1.2.3

---
+++ src/main/java/com/hvdbs/savra/stepicalgorithmicandprogramming/Chapter1/Unit2/Task_03.java
@@ -0,0 +1,32 @@
+package com.hvdbs.savra.stepicalgorithmicandprogramming.Chapter1.Unit2;
+
+import java.util.Scanner;
+
+/**
+ На вход подаётся одна строка длиною не менее двух и не более 100 символов.
+ На выход подайте первые два символа строки, написанные без пробела.
+
+ Например.
+ Вход:
+ Строка
+ Выход:
+ Ст
+
+ Sample Input 1:
+ Пи=3.14159
+ Sample Output 1:
+ Пи
+
+ Sample Input 2:
+ Python
+ Sample Output 2:
+ Py
+*/
+public class Task_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+
+        System.out.println(s.substring(0, 2));
+    }
+}
